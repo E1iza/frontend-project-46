a86d4232172fb4a1e8ef082a99eac6a51159c223
eliza nov <eliza.novizkaya@gmail.com>
2024-04-09 20:52:35 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,12 @@ import { program } from 'commander';
 program
   .name('')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  // .option('-h, --help')
+  // .option('-V, --version')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 // program.command('genDiff')
 //   .description('Команда выводит разницу между файлами')
