11cd96ab47c8cc5e70b9008cd081852c66d16528
eliza nov <eliza.novizkaya@gmail.com>
2024-04-09 20:40:19 +0300

    обновлен вывод справки

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
-const genDiff = (filepath1, filepath2) => {};
+// const genDiff = (filepath1, filepath2) => {};
 
 program
   .name('')
-  .description('Инструмент определяющий разницу между файлами')
+  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
 
-program.command('genDiff')
-  .description('Команда выводит разницу между файлами')
-  .argument('<filepath1>', 'первый файл')
-  .argument('<filepath2>', 'второй файл')
-  .option('-h, --help')
-  .option('-V, --version')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
-  });
+// program.command('genDiff')
+//   .description('Команда выводит разницу между файлами')
+//   .argument('<filepath1>', 'первый файл')
+//   .argument('<filepath2>', 'второй файл')
+//   .option('-h, --help')
+//   .option('-V, --version')
+//   .action((filepath1, filepath2) => {
+//     console.log(genDiff(filepath1, filepath2));
+//   });
 
 program.parse();
