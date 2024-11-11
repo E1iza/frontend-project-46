838d2eecd97d093ad59ed508e045e7d8c15c2619
eliza <eliza.novizkaya@gmail.com>
2024-11-11 19:14:46 +0300

    Исправление замечаний

--- src/index.js
+++ src/index.js
@@ -5,20 +5,21 @@ import getAst from './getAST.js';
 import stylish from './formatters/stylish.js';
 import plain from './formatters/plain.js';
 
-const getDiff = (filePath1, filePath2, style = 'stylish') => {
+const formatters = {
+  stylish,
+  plain,
+  json: JSON.stringify,
+};
+
+const getDiff = (filePath1, filePath2, format = 'stylish') => {
   const file1 = fs.readFileSync(filePath1, 'utf8');
   const file2 = fs.readFileSync(filePath2, 'utf8');
-  const type1 = path.extname(filePath1).slice(1);
-  const type2 = path.extname(filePath2).slice(1);
+  const type1 = path.extname(filePath1);
+  const type2 = path.extname(filePath2);
   const data1 = parsers(type1, file1);
   const data2 = parsers(type2, file2);
   const ast = getAst(data1, data2);
-  const formatters = {
-    stylish,
-    plain,
-    json: JSON.stringify,
-  };
-  return formatters[style](ast);
+  return formatters[format](ast);
 };
 
 export default getDiff;
--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,9 @@
 import yaml from 'js-yaml';
 
 const mapping = {
-  yml: yaml.load,
-  yaml: yaml.load,
-  json: JSON.parse,
+  '.yml': yaml.load,
+  '.yaml': yaml.load,
+  '.json': JSON.parse,
 };
 
 const parsers = (type, data) => mapping[type](data);
